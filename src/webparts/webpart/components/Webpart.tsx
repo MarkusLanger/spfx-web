e9335207fb178bb8c040ec3c5589fcaf1db6a6d6
Markus Langer <markus.langer@gis-ag.com>
2021-05-06 09:10:01 +0200

    use React components

--- src/webparts/webpart/components/Webpart.tsx
+++ src/webparts/webpart/components/Webpart.tsx
@@ -2,28 +2,34 @@ import * as React from 'react';
 import styles from './Webpart.module.scss';
 import { IWebpartProps } from './IWebpartProps';
 import { escape } from '@microsoft/sp-lodash-subset';
-import { LibraryComponentLibrary } from "spfx-lib";
+import { LibraryComponentLibrary, Placeholder, MyDiv } from 'spfx-lib';
 
 export default class Webpart extends React.Component<IWebpartProps, {}> {
-  public render(): React.ReactElement<IWebpartProps> {
-    const lib = new LibraryComponentLibrary();
-    const name = lib.name();
-    console.log('### name', name);
-    return (
-      <div className={ styles.webpart }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more: {name}</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    public render(): React.ReactElement<IWebpartProps> {
+        const lib = new LibraryComponentLibrary();
+        const name = lib.name();
+        console.log('### name', name);
+        return (
+            <React.Fragment>
+                <div className={styles.webpart}>
+                    <div className={styles.container}>
+                        <div className={styles.row}>
+                            <div className={styles.column}>
+                                <span className={styles.title}>Welcome to SharePoint!</span>
+                                <p className={styles.subTitle}>Customize SharePoint experiences using Web Parts.</p>
+                                <p className={styles.description}>{escape(this.props.description)}</p>
+                                <a href="https://aka.ms/spfx" className={styles.button}>
+                                    <span className={styles.label}>Learn more: {name}</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <Placeholder />
+                <hr />
+                <MyDiv />
+            </React.Fragment>
+        );
+    }
 }
